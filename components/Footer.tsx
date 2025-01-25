a34e1cb3e8f607c57d5424b9acca487ef0ecc316
chrytory <1md10119w@gmail.com>
2025-01-25 23:28:24 +0900

    test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,19 +3,20 @@ import ResponsiveText from "@/components/ResponsiveText"
 
 export default function Footer() {
   return (
-    <ResponsiveText>
     <footer className="bg-ossan-header-footer text-ossan-text">
       <div className="container mx-auto px-4 py-6">
         <div className="flex flex-col items-center md:flex-row md:justify-between md:items-start gap-4">
           <div className="text-center md:text-left">
-            <h2 className="text-2xl font-bold mb-2 font-serif">
+          <ResponsiveText>
+          <h2 className="text-2xl font-bold mb-2 font-serif">
               <span className="word-wrapper whitespace-pre">おっさんず・ガーデン</span>
-            </h2>
+              </h2>
             <p className="text-sm">
             <span className="word-wrapper whitespace-pre">やりたいときに、</span>
             <span className="word-wrapper whitespace-pre">やりたいことを、</span>
             <span className="word-wrapper whitespace-pre">やりたいだけやる</span>
             </p>
+            </ResponsiveText>
           </div>
           <div className="text-center md:text-right space-y-1">
             <p>LINE: XXXXXXX</p>
@@ -24,15 +25,16 @@ export default function Footer() {
           </div>
         </div>
         <div className="mt-4 text-center">
-          <p className="text-sm">
-          <span className="word-wrapper whitespace-pre">&copy; 2024 </span>
+        <ResponsiveText>
+        <p className="text-sm">
+        <span className="word-wrapper whitespace-pre">&copy; 2024 </span>
           <span className="word-wrapper whitespace-pre">おっさんず・ガーデン. </span>
           <span className="word-wrapper whitespace-pre">All rights reserved.</span>
           </p>
-        </div>
+          </ResponsiveText>
+          </div>
       </div>
     </footer>
 
-    </ResponsiveText>
   )
 }
